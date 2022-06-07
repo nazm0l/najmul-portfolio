74a9911438522a5071e84e2ba917d71a8af4c8ba
nazm0l <tanjirahmednazmul@gmail.com>
2022-06-07 07:56:03 +0600

    Initial setup and components added

---
+++ src/components/Skills/Skills.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Skills.css'
+
+const Skills = () => {
+    return (
+        <div>
+            <h2>Skills</h2>
+        </div>
+    );
+};
+
+export default Skills;
